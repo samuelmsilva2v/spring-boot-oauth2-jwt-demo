73339eddcf0ce71224bf89d8ca3a2ea54efa8685
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-08-26 18:53:34 -0300

    bcrypt password encoder

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$qvKX1BPV63DGdqK2ZH0fx.17.TkNU47weTfgGRyilEIKy0ifLLMDu');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$qvKX1BPV63DGdqK2ZH0fx.17.TkNU47weTfgGRyilEIKy0ifLLMDu');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
